ace43eca72a97b9e7b2230bbba8cc4320fc3ff31
Victor Macias <v.maciascastillo@gmail.com>
2016-11-22 16:20:35 +0100

    article manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # List all articles
+  get 'articles', to: "articles#index"
+
+  # Create a new article
+  get 'articles/new', to: "articles#new", as: 'new'
+
+  post 'articles', to: 'articles#create'
+
+  # Show a single article
+  get 'articles/:id', to: "articles#show", as: 'article'
+
+  # Update article
+  get 'articles/:id/edit', to: "articles#edit", as: 'edit'
+
+  patch 'articles/:id', to: "articles#update"
+
+ # Delete a task
+ delete 'articles/:id', to: "articles#destroy", as: 'delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
